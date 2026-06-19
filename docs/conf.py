9a2bba20977cd3a63df6e29011a7745360d93e02
Aned Esquerra Arguelles <aned.ezquerra@gmail.com>
2026-06-18 19:31:17 -0600

    Configuring project name for Dataviz

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,14 @@
+#-*- coding: utf-8 -*-
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..')) # Apbackground a la raíz de tu paquete
+
+# Project information
+project = 'Data-Viz'
+copyright = '2026, Aned Esquerra Arguelles' # Update year/name as you prefer
+author = 'Aned Esquerra Arguelles'
+release = '0.1.0'
 
 extensions = [
     'sphinx.ext.autodoc',  # Extrae docstrings automáticamente
