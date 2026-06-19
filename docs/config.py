047e386e38be77adf8bb4892ee7b8e92fcdf5ddb
Aned Esquerra Arguelles <aned.ezquerra@gmail.com>
2026-06-18 19:13:09 -0600

    Adding workflows and docs generation

---
+++ docs/config.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..')) # Apbackground a la raíz de tu paquete
+
+extensions = [
+    'sphinx.ext.autodoc',  # Extrae docstrings automáticamente
+    'sphinx.ext.napoleon', # Soporta formatos Google/NumPy
+]
+html_theme = 'sphinx_rtd_theme' # El tema clásico azul/gris de Mlxtend
